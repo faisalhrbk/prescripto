73cd8d04b50718c31d1ef1c1518ae791059f0f58
faisalhrbk <faisalhrbk@outlook.com>
2025-05-05 23:41:27 +0500

    used first time useNavigate

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,34 +1,40 @@
 import React from "react";
 import { assets } from "../assets/assets";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
-    return (
-			<div className="flex items-center  justify-between text-sm py-4 mb-5 border-b border-b-gray-400">
-				<img className="w-44 cursor-pointer" src={assets.logo} alt="" />
-				<ul className="hidden md:flex items-start gap-5 font-medium ">
-					<NavLink to="/">
-						<li className="py-1">HOME</li>
-						<hr className="hidden border-none outline-none h-0.5 bg-blue-500m-auto" />
-					</NavLink>
-					<NavLink to="/doctors">
-						<li className="py-1">All Doctors</li>
-						<hr className="hidden border-none outline-none h-0.5 bg-blue-500 m-auto" />
-					</NavLink>
-					<NavLink to="/about">
-						<li className="py-1">About</li>
-						<hr className="hidden border-none outline-none h-0.5 bg-blue-500 m-auto" />
-					</NavLink>
-					<NavLink to="/contact">
-						<li className="py-1">Contact</li>
-						<hr className="hidden border-none outline-none h-0.5 bg-blue-500 m-auto" />
-					</NavLink>
-				</ul>
-				<div>
-					<button>Create Account</button>
-				</div>
+	const navigate = useNavigate();
+	return (
+		<div className="flex items-center  justify-between text-sm py-4 mb-5 border-b border-b-gray-400">
+			<img className="w-44 cursor-pointer" src={assets.logo} alt="" />
+			<ul className="hidden md:flex items-start gap-5 font-medium ">
+				<NavLink to="/">
+					<li className="py-1">HOME</li>
+					<hr className="hidden border-none outline-none h-0.5 bg-blue-500m-auto" />
+				</NavLink>
+				<NavLink to="/doctors">
+					<li className="py-1">All Doctors</li>
+					<hr className="hidden border-none outline-none h-0.5 bg-blue-500 m-auto" />
+				</NavLink>
+				<NavLink to="/about">
+					<li className="py-1">About</li>
+					<hr className="hidden border-none outline-none h-0.5 bg-blue-500 m-auto" />
+				</NavLink>
+				<NavLink to="/contact">
+					<li className="py-1">Contact</li>
+					<hr className="hidden border-none outline-none h-0.5 bg-blue-500 m-auto" />
+				</NavLink>
+			</ul>
+			<div className="flex items-center gap-4">
+				<button
+					onClick={() => navigate("/login")}
+					className="bg-blue-500  text-white px-8 py-3 rounded-full font-light hidden md:block"
+				>
+					Create Account
+				</button>
 			</div>
-		);
+		</div>
+	);
 };
 
 export default Navbar;
