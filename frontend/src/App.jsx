60b1516a47005318b70f5edf80c20d01c4cbe35d
faisalhrbk <faisalhrbk@outlook.com>
2025-05-05 22:44:10 +0500

    created routes in react for SPA

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Doctors from "./pages/Doctors";
+import Login from "./pages/Login";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import MyProfile from "./pages/MyProfile";
+import MyAppointments from "./pages/MyAppointments";
+import Appointment from "./pages/Appointment";
 
 const App = () => {
-  return <h1 class="text-3xl font-bold underline">Hello world!</h1>;
-}
+	return (
+		<div className="mx-4 sm:mx-[10%]">
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/doctors" element={<Doctors />} />
+				<Route path="/doctors/:speciality" element={<Doctors />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/contact" element={<Contact />} />
+				<Route path="/my-profile" element={<MyProfile />} />
+				<Route path="/my-appointments" element={<MyAppointments />} />
+				<Route path="/appointment" element={<Appointment />} />
+			</Routes>
+		</div>
+	);
+};
 
-export default App
+export default App;
