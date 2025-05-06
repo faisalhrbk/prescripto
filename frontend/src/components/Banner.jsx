ffe0b485c1830754220f91ceb7b4e575426c649c
faisalhrbk <faisalhrbk@outlook.com>
2025-05-06 20:21:40 +0500

    created the banner image for home page

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import { assets } from "./../assets/assets";
+import { useNavigate } from 'react-router-dom';
 
 const Banner = () => {
-  return (
-    <div>Banner</div>
-  )
-}
+  const navigate = useNavigate()
+	return (
+		<div className="flex bg-blue-500 rounded-lg px-6 sm:px-10 md:p x-14 lg:px-12 my-20 ">
+			{/* left side  */}
+			<div className="flex-1 py-8 sm:py-10 md:py-16 lg:py-24 lg:pl-5">
+				<div className="tex-xl sm:text-2xl md:text-3xl lg:text-5xl font-semibold text-white">
+					<p>Book Appointment</p>
+					<p className="mt-4">with 100+ Trusted Doctors</p>
+				</div>
+      <button onClick={()=> {navigate('/login'); scrollTo(0,0)}} className="bg-white text-sm sm:text-base text-gray-600 px-8 py-6 rounded-full mt-6 hover:scale-105 transition-all">Create Account</button>
+			</div>
+			{/* right side  */}
+			<div className="hidden md:block md:w-1/2 lg:w-[370px] relative">
+				<img className="absolute w-full bottom-0 right-0 max-w-md" src={assets.appointment_img} alt="" />
+			</div>
+		</div>
+	);
+};
 
-export default Banner
+export default Banner;
