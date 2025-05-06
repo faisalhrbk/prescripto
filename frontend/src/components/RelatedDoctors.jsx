aac7dd61f8b769dca6e5484ba33d1fd7d10990e1
faisalhrbk <faisalhrbk@outlook.com>
2025-05-07 00:31:18 +0500

    im done today

---
+++ frontend/src/components/RelatedDoctors.jsx
@@ -0,0 +1,12 @@
+import React, { useContext, useState } from 'react'
+import { AppContext } from '../context/AppContext';
+
+const RelatedDoctors = () => {
+    const {doctors} = useContext(AppContext);
+    const [relDoc, setRelDocs] useState
+  return (
+    <div>RelatedDoctors</div>
+  )
+}
+
+export default RelatedDoctors
