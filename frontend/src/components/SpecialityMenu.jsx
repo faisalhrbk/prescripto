64ec63cc30b8c6812115e2c8876fc6fbad1fe08e
faisalhrbk <faisalhrbk@outlook.com>
2025-05-06 14:52:19 +0500

    bro this is soo hard

--- frontend/src/components/SpecialityMenu.jsx
+++ frontend/src/components/SpecialityMenu.jsx
@@ -4,16 +4,19 @@ import { Link } from "react-router-dom";
 
 const SpecialityMenu = () => {
 	return (
-		<div id="speciality">
-			<h1>Find By Speciality</h1>
-			<p>
+		<div
+			id="speciality"
+			className="flex flex-col items-center gap-4 py-16 text-gray-600"
+		>
+			<h1 className="text-3xl font-medium">Find By Speciality</h1>
+			<p className="sm:w-1/3 text-center text-sm">
 				Simply Browse through our extensive list of trusted doctors, schedule
-				your appointment today !
+				your appointment hassle free!
 			</p>
-			<div>
+			<div className="flex sm:justify-center gap-4 pt-5 w-full overflow-scroll">
 				{specialityData.map((item, index) => (
-					<Link key={index} to={`/doctors/${item.speciality}`}>
-						<img src={item.image} alt="" />
+					<Link className="flex flex-col items-center text-xs cursor-pointer flex-shrink-0 hover:translate-y-[-10px] transition-all duration-500" key={index} to={`/doctors/${item.speciality}`}>
+						<img className="w-16 sm:w-24 mb-2" src={item.image} alt="" />
 						<p>{item.speciality}</p>
 					</Link>
 				))}
