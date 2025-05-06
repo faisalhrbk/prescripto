b4d0a9225d3b0ab4553210cef8b090579f43915a
faisalhrbk <faisalhrbk@outlook.com>
2025-05-06 23:11:52 +0500

    this is totally insane bruhh i fucking hate react js

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -2,9 +2,12 @@ import { createContext } from "react";
 import { doctors } from "../assets/assets";
 export const AppContext = createContext();
 const AppContextProvider = (props) => {
+	const currencySymbol = "$";
 	const value = {
 		doctors,
+		currencySymbol,
 	};
+
 	return (
 		<AppContext.Provider value={value}>{props.children}</AppContext.Provider>
 	);
