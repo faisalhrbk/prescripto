b287b3cc02505f7b66cc73d1f7d8a946210a3498
faisalhrbk <faisalhrbk@outlook.com>
2025-05-08 16:14:20 +0500

    working on responsiveness i really hate this part

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,11 +4,16 @@ import { NavLink, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
 	const navigate = useNavigate();
-	// const [showMenu, setShowMenu] = useState(false);
+	const [showMenu, setShowMenu] = useState(false);
 	const [token, setToken] = useState(true);
 	return (
 		<div className="flex items-center  justify-between text-sm py-4 mb-5 border-b border-b-gray-400">
-			<img onClick={()=>navigate('/')} className="w-44 cursor-pointer" src={assets.logo} alt="" />
+			<img
+				onClick={() => navigate("/")}
+				className="w-44 cursor-pointer"
+				src={assets.logo}
+				alt=""
+			/>
 			<ul className="hidden md:flex items-start gap-5 font-medium ">
 				<NavLink to="/">
 					<li className="py-1">HOME</li>
@@ -63,6 +68,34 @@ const Navbar = () => {
 						Create Account
 					</button>
 				)}
+				<img
+					onClick={() => setShowMenu(true)}
+					className="w-6 md:hidden "
+					src={assets.menu_icon}
+					alt=""
+				/>
+				{/* mobile view */}
+				<div
+					className={` ${
+						showMenu ? "fixed w-full" : "h-0 w-0"
+					}md:hidden right-0 top-0 bottom-0 overflow-hidden bg-white transition-all`}
+				>
+					<div className="flex items-center justify-between px-5 py-6">
+						<img className="w-36" src={assets.logo} alt="" />
+						<img
+							className="w-7"
+							onClick={() => setShowMenu(false)}
+							src={assets.cross_icon}
+							alt=""
+						/>
+					</div>
+					<ul className="flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium">
+						<NavLink to="/">Home</NavLink>
+						<NavLink to="/doctors">All Doctors</NavLink>
+						<NavLink to="/about"> About</NavLink>
+						<NavLink to="/contact">Contact</NavLink>
+					</ul>
+				</div>
 			</div>
 		</div>
 	);
