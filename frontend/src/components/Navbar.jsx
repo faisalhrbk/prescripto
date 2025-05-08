0febe52dbc46dc623b08fe4fce66d7fc1b27b718
faisalhrbk <faisalhrbk@outlook.com>
2025-05-08 16:22:26 +0500

    i don;t get this shit bruhh

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -90,10 +90,18 @@ const Navbar = () => {
 						/>
 					</div>
 					<ul className="flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium">
-						<NavLink to="/">Home</NavLink>
-						<NavLink to="/doctors">All Doctors</NavLink>
-						<NavLink to="/about"> About</NavLink>
-						<NavLink to="/contact">Contact</NavLink>
+						<NavLink onClick={() => setShowMenu(false)} to="/">
+							<p className="px-4 py-2 rounded inline-block">Home</p>
+						</NavLink>
+						<NavLink onClick={() => setShowMenu(false)} to="/doctors">
+							<p className="px-4 py-2 rounded inline-block"> All Doctors</p>
+						</NavLink>
+						<NavLink onClick={() => setShowMenu(false)} to="/about">
+							<p className="px-4 py-2 rounded inline-block"> About</p>
+						</NavLink>
+						<NavLink onClick={() => setShowMenu(false)} to="/contact">
+							<p className="px-4 py-2 rounded inline-block"> Contact</p>
+						</NavLink>
 					</ul>
 				</div>
 			</div>
