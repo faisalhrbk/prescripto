18e8b9f650505320041636de24410bad4562f597
faisalhrbk <faisalhrbk@outlook.com>
2025-05-08 19:07:56 +0500

    created routes and used multer for multipart form data destination isnt yet been specfired

---
+++ backend/controllers/adminController.js
@@ -0,0 +1,17 @@
+//Api fro adding doctor
+
+export const addDoctor = async (req, res) => {
+	try {
+		const {
+			name,
+			email,
+			password,
+			speciality,
+			degree,
+			experience,
+			about,
+			fees,
+			address,
+		} = req.body;
+	} catch (err) {}
+};
